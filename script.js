f185760cd141aab3b5e5bbaccf60df152f708501
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2024-11-20 18:58:38 -0300

    Primeiro commit, versão inicial do projeto

---
+++ script.js
@@ -0,0 +1,34 @@
+const rockButton = document.querySelector("#rock")
+const paperButton = document.querySelector("#paper")
+const scissorButton = document.querySelector("#scissors")
+const result = document.querySelector(".result")
+const playerScore = document.querySelector(".player")
+const computerScore = document.querySelector(".computer")
+
+function computerChoice() {
+    const comChoice = Math.random() * 3
+    if (comChoice >= 0 && comChoice < 1) {
+        return "rock"
+    } else if (comChoice >= 1 && comChoice < 2) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+function startGame(choice) {
+    const comChoice = computerChoice()
+    if (choice === comChoice) {
+        result.innerHTML = "Vocês empataram !!"
+    } else if ((choice === "rock" && comChoice === "paper") || (choice === "paper" && comChoice === "scissors") || (choice === "scissors" && comChoice === "rock")) {
+        result.innerHTML = "Você perdeu !!"
+        computerScore.innerHTML++
+    } else {
+        result.innerHTML = "Você ganhou !!"
+        playerScore.innerHTML++
+    }
+}
+
+rockButton.addEventListener("click", () => startGame("rock"))
+paperButton.addEventListener("click", () => startGame("paper"))
+scissorButton.addEventListener("click", () => startGame("scissors"))
